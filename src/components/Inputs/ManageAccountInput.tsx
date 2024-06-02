4434beec05f718e569a4acbc4ce5a428019c0193
TusharSatam <tsatam91@gmail.com>
2024-06-02 21:46:37 +0530

    manage account screen  form  ui

---
+++ src/components/Inputs/ManageAccountInput.tsx
@@ -0,0 +1,183 @@
+import React, { useState } from 'react';
+import { StyleSheet, TextInput, View, TouchableOpacity, Modal, Text, Platform } from 'react-native';
+import CustomText from '../Text/CustomText';
+import DatePicker from 'react-native-date-picker';
+import RNPickerSelect from 'react-native-picker-select';
+
+interface ManageAccountInputProps {
+    placeholderText: string;
+    label: string;
+    inputType?: 'default' | 'email-address' | 'phone-pad' | 'date' | 'select';
+    value: string | Date;
+    onChange: (value: string | Date) => void;
+    error?: string | null;
+}
+
+const ManageAccountInput: React.FC<ManageAccountInputProps> = ({
+    placeholderText,
+    label,
+    inputType = 'default',
+    value,
+    onChange,
+    error
+}) => {
+    const [date, setDate] = useState<Date>(new Date(value));
+    const [open, setOpen] = useState<boolean>(false);
+    const isDateInput = inputType === 'date';
+    const isSelectInput = inputType === 'select';
+
+    const handleDateChange = (newDate: Date) => {
+        setDate(newDate);
+        onChange(newDate);
+    };
+
+    return (
+        <View style={styles.container}>
+            {label && <CustomText style={styles.inputLabel}>{label}</CustomText>}
+            {isDateInput ? (
+                <>
+                    <TouchableOpacity
+                        style={styles.input}
+                        onPress={() => setOpen(true)}
+                    >
+                        <CustomText style={{ color: value ? '#242424' : '#797979' }}>
+                            {value ? new Date(value).toDateString() : placeholderText}
+                        </CustomText>
+                    </TouchableOpacity>
+                    <Modal
+                        transparent={true}
+                        animationType="slide"
+                        visible={open}
+                        onRequestClose={() => setOpen(false)}
+                    >
+                        <View style={styles.modalContainer}>
+                            <View style={styles.modalContent}>
+                                <DatePicker
+                                    date={date}
+                                    onDateChange={handleDateChange}
+                                    mode="date"
+                                    maximumDate={new Date()} // Ensures date picker only allows past dates
+                                />
+                                <TouchableOpacity
+                                    onPress={() => setOpen(false)}
+                                    style={styles.closeButton}
+                                >
+                                    <CustomText style={styles.closeButtonText}>Done</CustomText>
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+                    </Modal>
+                </>
+            ) : isSelectInput ? (
+                <RNPickerSelect
+                    style={{
+                        inputIOS: styles.input,
+                        inputAndroid: styles.input,
+                    }}
+                    placeholder={{
+                        label: placeholderText,
+                        value: null,
+                    }}
+                    value={value as string}
+                    onValueChange={onChange}
+                    items={[
+                        { label: 'Male', value: 'male' },
+                        { label: 'Female', value: 'female' },
+                    ]}
+                    useNativeAndroidPickerStyle={false} // only for Android
+                    Icon={() => {
+                        return Platform.OS === 'ios' ? (
+                            <View style={styles.pickerIcon} />
+                        ) : (
+                            <View style={styles.pickerIconAndroid} />
+                        );
+                    }}
+                />
+            ) : (
+                <TextInput
+                    style={styles.input}
+                    placeholderTextColor="lightgray"
+                    placeholder={placeholderText ? placeholderText : ""}
+                    keyboardType={inputType === 'email-address' ? 'email-address' : inputType === "phone-pad" ? "phone-pad" : 'default'}
+                    value={value as string}
+                    onChangeText={onChange}
+                />
+            )}
+            {error && <CustomText style={styles.errorText}>{error}</CustomText>}
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'column',
+        marginBottom: 16,
+        display: 'flex',
+        gap: 10,
+    },
+    inputLabel: {
+        fontSize: 13,
+        fontWeight: '500',
+    },
+    input: {
+        width: '100%',
+        backgroundColor: '#f6f6f6',
+        paddingVertical: 10,
+        paddingHorizontal: 16,
+        fontSize: 14,
+        fontWeight: '400',
+        borderRadius: 10,
+        color: '#242424',
+    },
+    modalContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    },
+    modalContent: {
+        backgroundColor: 'black',
+        padding: 20,
+        borderRadius: 10,
+        alignItems: 'center',
+        width:"80%"
+    },
+    closeButton: {
+        marginTop: 20,
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        backgroundColor: '#FF4D67',
+        borderRadius: 10,
+    },
+    closeButtonText: {
+        color: 'white',
+        fontWeight: '500',
+    },
+    errorText: {
+        color: 'red',
+        fontSize: 12,
+    },
+    pickerIcon: {
+        borderTopWidth: 10,
+        borderTopColor: '#00000020',
+        borderBottomWidth: 10,
+        borderBottomColor: '#00000020',
+        borderLeftWidth: 10,
+        borderLeftColor: 'transparent',
+        borderRightWidth: 10,
+        borderRightColor: 'transparent',
+        width: 0,
+        height: 0,
+        marginTop: 8,
+        marginRight: 10,
+    },
+    pickerIconAndroid: {
+        backgroundColor: '#f6f6f6',
+        width: 12,
+        height: 12,
+        top: 20,
+        right: 15,
+    },
+});
+
+export default ManageAccountInput;
--- src/screens/SettingsScreens/ManageAccount.tsx
+++ src/screens/SettingsScreens/ManageAccount.tsx
@@ -1,11 +1,172 @@
-import CustomText from '@social/components/Text/CustomText'
-import React from 'react'
-import { View } from 'react-native'
+import React, { useState } from 'react';
+import { StyleSheet, View, ScrollView, Button, Alert } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import ManageAccountInput from '@social/components/Inputs/ManageAccountInput';
+import ScreenHeader from '@social/components/ScreenHeader/ScreenHeader';
+import PrimaryBtn from '@social/components/Buttons/PrimaryBtn';
 
-const ManageAccount = () => {
-    return (
-        <View><CustomText>ManageAccount</CustomText></View>
-    )
+interface FormState {
+    name: string;
+    username: string;
+    phoneNumber: string;
+    email: string;
+    dob: Date;
+    gender: string;
 }
 
-export default ManageAccount
+interface ErrorsState {
+    [key: string]: string | null;
+}
+
+const ManageAccount: React.FC = () => {
+    const navigation = useNavigation();
+
+    const [form, setForm] = useState<FormState>({
+        name: '',
+        username: '',
+        phoneNumber: '',
+        email: '',
+        dob: new Date(),
+        gender: '',
+    });
+
+    const [errors, setErrors] = useState<ErrorsState>({});
+
+    const handleChange = (field: keyof FormState, value: string | Date) => {
+        setForm({ ...form, [field]: value });
+        if (errors[field]) {
+            setErrors({ ...errors, [field]: null });
+        }
+    };
+
+    const validateForm = (): boolean => {
+        let valid = true;
+        let errors: ErrorsState = {};
+
+        if (!form.name) {
+            errors.name = "Name is required";
+            valid = false;
+        } else if (/\d/.test(form.name)) {
+            errors.name = "Name should not contain numbers";
+            valid = false;
+        }
+
+        if (!form.username) {
+            errors.username = "Username is required";
+            valid = false;
+        }
+
+        if (!form.phoneNumber) {
+            errors.phoneNumber = "Phone Number is required";
+            valid = false;
+        } else if (!/^\d{10}$/.test(form.phoneNumber)) {
+            errors.phoneNumber = "Phone Number should be 10 digits";
+            valid = false;
+        }
+
+        if (!form.email) {
+            errors.email = "Email is required";
+            valid = false;
+        } else if (!/\S+@\S+\.\S+/.test(form.email)) {
+            errors.email = "Email is invalid";
+            valid = false;
+        }
+
+        if (!form.dob) {
+            errors.dob = "Date of Birth is required";
+            valid = false;
+        }
+
+        if (!form.gender) {
+            errors.gender = "Gender is required";
+            valid = false;
+        }
+
+        setErrors(errors);
+        return valid;
+    };
+
+    const handleSubmit = () => {
+        if (validateForm()) {
+            Alert.alert("Form submitted successfully!");
+        } else {
+        }
+    };
+
+    return (
+        <View style={styles.mainContainer}>
+            <ScreenHeader headerName='Manage Account' navigation={navigation} />
+            <ScrollView style={styles.detailWrapper}>
+                <View style={styles.scrollView}>
+                    <ManageAccountInput
+                        label={"Name"}
+                        placeholderText={"Enter your name"}
+                        inputType="default"
+                        value={form.name}
+                        onChange={(value) => handleChange('name', value)}
+                        error={errors.name}
+                    />
+                    <ManageAccountInput
+                        label={"Username"}
+                        placeholderText={"Enter your username"}
+                        inputType="default"
+                        value={form.username}
+                        onChange={(value) => handleChange('username', value)}
+                        error={errors.username}
+                    />
+                    <ManageAccountInput
+                        label={"Phone Number"}
+                        placeholderText={"Enter your phone number"}
+                        inputType="phone-pad"
+                        value={form.phoneNumber}
+                        onChange={(value) => handleChange('phoneNumber', value)}
+                        error={errors.phoneNumber}
+                    />
+                    <ManageAccountInput
+                        label={"Email"}
+                        placeholderText={"Enter your email"}
+                        inputType="email-address"
+                        value={form.email}
+                        onChange={(value) => handleChange('email', value)}
+                        error={errors.email}
+                    />
+                    <ManageAccountInput
+                        label={"DOB"}
+                        placeholderText={"Select your date of birth"}
+                        inputType="date"
+                        value={form.dob}
+                        onChange={(value) => handleChange('dob', value)}
+                        error={errors.dob}
+                    />
+                    <ManageAccountInput
+                        label={"Gender"}
+                        placeholderText={"Select your gender"}
+                        inputType="select"
+                        value={form.gender}
+                        onChange={(value) => handleChange('gender', value)}
+                        error={errors.gender}
+                    />
+                </View>
+                <PrimaryBtn btnText='Update Profile' onPress={handleSubmit} btnClass={"mb-10"}/>
+            </ScrollView>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    mainContainer: {
+        flex: 1,
+        backgroundColor: '#fff',
+        paddingHorizontal: 16,
+    },
+    detailWrapper: {
+        flex: 1,
+        paddingTop: 20,
+        width: '100%',
+    },
+    scrollView: {
+        flex: 1,
+    },
+});
+
+export default ManageAccount;
